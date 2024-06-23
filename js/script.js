d91c95d7d4b3054172d4c17c9129473add728f55
M4theusj <matheusjacob312@hotmail.com>
2024-06-23 14:43:16 -0300

    feat: PokedexRework, interface quase finalizada

--- js/script.js
+++ js/script.js
@@ -1,176 +1,39 @@
-const BASEURL = "https://pokeapi.co/api/v2/pokemon/"
+const BASEURL = "https://pokeapi.co/api/v2/pokemon/";
 
-document.addEventListener("DOMContentLoaded", getPokemonList)
+document.addEventListener("DOMContentLoaded", getPokemonList);
 
 function getPokemonList() {
-    fetch(BASEURL + '?limit=251')
-        .then(response => {
-            if (response.status === 200) {
-                return response.json()
-            }
-        })
+    fetch(BASEURL + '?limit=1025')
+        .then(response => response.json())
         .then(data => {
             data.results.forEach(pokemon => {
-                displayPokemon(pokemon)
+                displayPokemon(pokemon);
             });
         })
-        .catch(error => console.error(error))
-}
-
-function getPokemonDetails(id) {
-    if (id <= 251) {
-        const url = `${BASEURL}${id}/`
-        return fetch(url)
-            .then(response => {
-                if (response.status === 200) {
-                    return response.json()
-                } 
-            });
-    } else {
-        alert ("ID inválido ( 1 - 251 ) ")
-    }
-}
-
-function showPokemonDetails(id) {
-    getPokemonDetails(id)
-        .then(pokemon => {
-            showPokemonDetailsModal(pokemon)
-        });
-}
-
-function showPokemonByIdPrompt() {
-    const pokemonId = prompt("Digite o ID do Pokémon:")
-    if (pokemonId) {
-        showPokemonDetails(pokemonId);
-    }
+        .catch(error => console.error(error));
 }
 
 function displayPokemon(pokemon) {
-    let liPokemon = document.createElement("li");
-    let liPokemonImg = document.createElement("img");
-    let pokeballIcon = document.createElement("img"); 
+    const pokemonList = document.getElementById("ListaPokemons");
 
+    const listItem = document.createElement("li");
+
+    const pokemonImage = document.createElement("img");
     const pokemonId = pokemon.url.split('/')[6];
 
-    liPokemonImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
-    liPokemon.innerHTML = `${pokemon.name} - ${pokemonId}`;
-
-    liPokemon.setAttribute('data-pokemon-id', pokemonId);
-
-    pokeballIcon.src = "img/Pokebola2.png";
-    pokeballIcon.className = "pokeball-icon";
-    pokeballIcon.onclick = function () {
-        addToTeam(pokemonId);
-    };
-
-    const detailsButton = document.createElement("button");
-    detailsButton.textContent = "Detalhes";
-    detailsButton.addEventListener("click", function () {
-        const clickedPokemonId = liPokemon.getAttribute('data-pokemon-id');
-        showPokemonDetails(clickedPokemonId);
-    });
-
-    liPokemon.appendChild(liPokemonImg);
-    liPokemon.appendChild(pokeballIcon); 
-    liPokemon.appendChild(detailsButton);
-
-    document.getElementById("ListaPokemons").appendChild(liPokemon);
-}
-
-
-
-
-function showPokemonDetailsModal(pokemon) {
-    const modalContent = document.getElementById("modalContent")
-    modalContent.innerHTML = `<h2 id="pokemonName">${pokemon.name}</h2>`
-
+    pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
     
-    const detailsContainer = document.createElement("div")
-    detailsContainer.id = "pokemonDetails"
-    detailsContainer.innerHTML = `
-        <div class="pokemon-details-inner">
-            <img src="${pokemon.sprites.front_default}" alt="Imagem de ${pokemon.name}" class="pokemon-image">
-            <div class="details-text">
-                <strong>Tipo:</strong> ${pokemon.types.map(type => type.type.name).join(" | ")}<br>
-                <strong>Altura:</strong> ${pokemon.height / 10} m<br>
-                <strong>Peso:</strong> ${pokemon.weight / 10} kg<br>
-                <strong>Habilidade Regular:</strong> ${pokemon.abilities[0].ability.name}
-            </div>
-        </div>
-    `
-    modalContent.appendChild(detailsContainer)
+    const pokemonName = document.createElement("div");
+    pokemonName.textContent = `${pokemon.name} - ${pokemonId}`;
+    pokemonName.className = "pokemon-name";
 
-    const addToTeamButton = document.createElement("button")
-    addToTeamButton.id = "addToTeamButton"
-    addToTeamButton.textContent = "Adicionar ao Time"
-    addToTeamButton.addEventListener("click", function () {
-        addToTeam(pokemon.id)
-        closeModal()
-    });
-    modalContent.appendChild(addToTeamButton)
+    const pokemonType = document.createElement("div");
+    pokemonType.textContent = "⚡";
+    pokemonType.className = "pokemon-type";
 
-    const modal = document.getElementById("pokemonModal")
-    modal.style.display = "block"
-}
+    listItem.appendChild(pokemonImage);
+    listItem.appendChild(pokemonName);
+    listItem.appendChild(pokemonType);
 
-function closeModal() {
-    const modal = document.getElementById("pokemonModal")
-    modal.style.display = "none"
-}
-
-function addToTeam(pokemonId) {
-    const teamList = document.getElementById("teamPokemonList")
-
-    const existingPokemon = teamList.querySelector(`[data-pokemon-id="${pokemonId}"]`)
-    if (existingPokemon) {
-        removeFromTeam(pokemonId)
-    } else {
-        if (teamList.childElementCount < 6) {
-            const teamPokemon = document.createElement("li")
-            const teamPokemonImg = document.createElement("img")
-
-            teamPokemonImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`
-
-            teamPokemon.setAttribute('data-pokemon-id', pokemonId)
-            teamPokemon.addEventListener("click", function () {
-                const clickedPokemonId = this.getAttribute('data-pokemon-id')
-                removeFromTeam(clickedPokemonId)
-            });
-
-            teamPokemon.appendChild(teamPokemonImg)
-            teamList.appendChild(teamPokemon)
-        } else {
-            alert("Você atingiu o limite de 6 Pokémon no time!")
-        }
-    }
-
-    
-    
-}
-
-function removeFromTeam(pokemonId) {
-    const teamList = document.getElementById("teamPokemonList")
-    const teamPokemon = document.querySelector(`#teamPokemonList [data-pokemon-id="${pokemonId}"]`)
-
-    if (teamPokemon) {
-        teamList.removeChild(teamPokemon)
-    }
-}
-
-function showPokemonList() {
-    document.getElementById("pokemonDetails").innerHTML = ""
-
-    fetch(BASEURL + '?limit=251')
-        .then(response => {
-            if (response.status === 200) {
-                return response.json()
-            }
-        })
-        .then(data => {
-            document.getElementById("ListaPokemons").innerHTML = ""
-
-            data.results.forEach(pokemon => {
-                displayPokemon(pokemon)
-            })
-        })
+    pokemonList.appendChild(listItem);
 }
